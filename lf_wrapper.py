570aaba0b5100a3370da2eb13e2c74db3faecac9
ghsieh <geoff.hsieh@laserfiche.com>
2017-12-12 16:13:22 -0800

    updated logic for pulling user/pass from environment.py

--- lf_wrapper.py
+++ lf_wrapper.py
@@ -225,8 +225,11 @@ class LfWrapper:
         
         #Function Logic Starts here
         #if args are not given pull from environment.py
-        arg_keys = ['server', 'database', 'username', 'password']
-        creds = tuple([GetDefaultCred(k, kwargs) for k in arg_keys])
+        server = GetDefaultCred('server', kwargs)
+        database = GetDefaultCred('database', kwargs)
+        username = GetDefaultCred('username') if not 'server' in kwargs or 'username' in kwargs else ''
+        password = GetDefaultCred('password') if not 'server' in kwargs or 'password' in kwargs else ''
+        creds = (server, database, username, password)
 
         sdk_loaded = self._sdk != None
         if sdk_loaded:
